7f77c5f1041236d2018264e0a7a3ea7beda0f3ee
Chronona <erewhon1872@yahoo.co.jp>
2020-07-29 11:18:55 +0900

    add : solution
    解答追加

--- py_checkio_solutions/Alice In Wonderland/middle_characters.py
+++ py_checkio_solutions/Alice In Wonderland/middle_characters.py
@@ -1,31 +1,24 @@
 #!/usr/local/bin/checkio --domain=py run middle-characters
 
-# You are given some string where you need to find its middle characters. The string may contain one word, a few symbols or a whole sentence. If the length of the string is even, then you should return two middle characters, but if the length is odd, return just one. For more details look at the Example.
-# 
-# 
-# 
-# Input:A string.
-# 
-# Output:The middle characters.
-# 
-# Precondition:
-# 1<= the length of the text<= 100
-# 
-# 
-# END_DESC
 
 def middle(text):
-    #replace this for solution
-    return text
+    if len(text) % 2 == 0:
+        middle_point = int(len(text) / 2 - 1)
+        return text[middle_point : middle_point + 2]
+    else:
+        text += "_"
+        middle_point = int(len(text) / 2 - 1)
+        return text[middle_point : middle_point + 1]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("Example:")
-    print(middle('example'))
+    print(middle("example"))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert middle('example') == 'm'
-    assert middle('test') == 'es'    
-    assert middle('very-very long sentence') == 'o'
-    assert middle('I') == 'I'
-    assert middle('no') == 'no'
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert middle("example") == "m"
+    assert middle("test") == "es"
+    assert middle("very-very long sentence") == "o"
+    assert middle("I") == "I"
+    assert middle("no") == "no"
     print("Coding complete? Click 'Check' to earn cool rewards!")
--- py_checkio_solutions/Alice In Wonderland/swap_nodes.py
+++ py_checkio_solutions/Alice In Wonderland/swap_nodes.py
@@ -1,22 +1,20 @@
 #!/usr/local/bin/checkio --domain=py run swap-nodes
 
-# Your task is to swap elements of the list (Iterable) pairwise. If you are given a list of 4 elements, then your function should return the same list, only in it the first and second elements are interchanged, as well as the third and fourth.
-# 
-# If there isn’t a paired amount of elements, then leave the last unpaired element in its place. An empty list should remain empty.
-# 
-# Input:Iterable.
-# 
-# Output:Iterable.
-# 
-# 
-# END_DESC
+import itertools
+
 
 def swap_nodes(a):
-    # your code here
-    return None
+    def split_list(l, n):
+        for i in range(0, len(l), n):
+            yield l[i : i + n]
+
+    result = list(split_list(a, 2))
+    result = [i[::-1] for i in result]
+    result = list(itertools.chain.from_iterable(result))
+    return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Example:")
     print(list(swap_nodes([1, 2, 3, 4])))
 
